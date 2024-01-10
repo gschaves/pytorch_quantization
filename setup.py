896e005c4795625ef54a5ff2f6a03e8a5b1fa185
Gabriel Chaves <gabriel.chaves@smt.ufrj.br>
2024-01-10 12:28:44 -0300

    create quant folder

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+README_LOCATION = "README.md"
+VERSION_FILE = "version.txt"
+INIT_FILE = "pytorch_quantization/__init__.py"
+
+def get_version():
+    with open(VERSION_FILE, encoding="utf-8") as f:
+        ver = f.readline()
+
+    if ver.startswith("for"):
+        with open(INIT_FILE, encoding="utf-8") as f:
+            for line in f.readlines():
+                if line.startswith("__version__"):
+                    ver = line.split()[-1].strip('"') + "+master"
+
+    return ver
+
+setup(
+    name='pytorch-quantization',
+    version=get_version(),
+    packages=find_packages(where="./pytorch_quantization")
+)
